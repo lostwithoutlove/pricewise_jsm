06afffafe2550e69c17e7a8d8525ebf7781fdebb
Abhilasha Purwar <abhilasha.purwar@gmail.com>
2024-04-06 22:00:56 +0530

    React Responsive Carousel

--- app/page.tsx
+++ app/page.tsx
@@ -6,8 +6,8 @@ import Herocarousel from "@/components/herocarousel";
 const Home = () => {
   return (
     <>
-      <section className="px-6 md:px-20 py-24 border-red-400 hover:bg-red-100">
-        <div className="flex max-xl:flex-col gap-16 border-red-400 hover:bg-red-100">
+      <section className="px-6 md:px-20 py-24">
+        <div className="flex max-xl:flex-col gap-16">
           <div className="flex flex-col justify-center border-red-400 hover:bg-red-100">
             <p className="small-text">
               Smart Shopping Starts Here:
@@ -30,7 +30,6 @@ const Home = () => {
           </div>
           <Herocarousel />
         </div>
-        <div>Home</div>
       </section>
       <section className="trending-section">
         <h2 className="section-text">Trending</h2>
--- components/searchbar.tsx
+++ components/searchbar.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+"use client";
+import { FormEvent, useState } from "react";
 
 const Searchbar = () => {
-  return <div>Searchbar</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const handleSubmit = () => {};
+
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      Searchbar
+      <input
+        type="text"
+        value="text"
+        onChange={() => {}}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button type="submit" className="searchbar-btn">
+        {isLoading ? "Searching..." : "Search"}
+      </button>
+    </form>
+  );
 };
 
 export default Searchbar;
